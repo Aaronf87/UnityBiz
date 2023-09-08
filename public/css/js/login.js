79f9482b747c50a054238a96e666bf543eab298a
Aaron Flores <97806359+Aaronf87@users.noreply.github.com>
2023-09-07 21:35:31 -0500

    update loginjs, loginhtml

---
+++ public/css/js/login.js
@@ -0,0 +1,7 @@
+// create login variables to getelementsbyid
+var signIn = document.getElementById('signIn');
+
+// create event listener for sign in button
+        signIn.addEventListener('click', () => {
+            console.log('You have successfully logged in!');
+        });
