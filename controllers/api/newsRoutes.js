cb2d9374c4f70c74c741c5fa8f94d74b33117108
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-09-07 19:21:23 -0500

    Created Initial Routes Structure

---
+++ controllers/api/newsRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+
+// ***WILL PASS IN MODELS THAT MATTHEW WILL MAKE
+// const {  } = require("../../models"); 
+
+// The `/api/news` endpoint
+
+module.exports = router;
---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const homeRoutes = require('./homeRoutes')
+const apiRoutes = require('./api');
+
+// Send all the requests that begin with `/` to homeRoutes.js.
+router.use('/', homeRoutes);
+
+// Send all the requests that begin with /api to the index.js in the api folder.
+router.use('/api', apiRoutes);
+
+// *** WILL ADD WILDCARD ROUTE LATER ***
