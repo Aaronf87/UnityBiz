3366c19b6111ed5ec32eb1fe372c4b14b853a639
keybit-1 <131838974+keybit-1@users.noreply.github.com>
2023-09-07 19:16:32 -0500

    added template models

--- models/user.js
+++ models/user.js
@@ -1,1 +1,17 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  // Add other fields like email, etc., as required
+});
+
+module.exports = User;
 
