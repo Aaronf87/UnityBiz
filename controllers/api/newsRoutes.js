d2ceb88bda20952d629ba769c9b4ce7ca17d5276
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-09-11 20:20:56 -0500

    Updated newsletter routes

--- controllers/api/newsRoutes.js
+++ controllers/api/newsRoutes.js
@@ -1,88 +1,77 @@
 const router = require('express').Router();
-const Newsletter = require('../../models/Newsletter');  // adjust the path to your Newsletter model
+const { Newsletter, Employee } = require('../../models');
 
-console.log("Imported Newsletter model:", Newsletter); // Log the imported Newsletter model
+// The `/api/news` endpoint
 
-// Create a new newsletter post
-router.post('/', async (req, res) => {
-  console.log("Request body:", req.body);  // Log the request body
-  try {
-    const newNewsletter = await Newsletter.create(req.body);
-    console.log("New newsletter created:", newNewsletter);  // Log the new newsletter
-    res.status(201).json(newNewsletter);
-  } catch (err) {
-    console.log("Error:", err);  // Log any errors
-    res.status(500).json(err);
-  }
-});
-
-// Get all newsletter posts
+// GET all newsletter posts
 router.get('/', async (req, res) => {
   try {
-    const newsletters = await Newsletter.findAll();
-    console.log("Found newsletters:", newsletters);  // Log the found newsletters
-    res.status(200).json(newsletters);
+    const newsData = await Newsletter.findAll({
+      order: [["createdAt", "DESC"]],
+      include: [{ model: Employee, attributes: ["first_name", "last_name"] }],
+    });
+    res.status(200).json(newsData);
   } catch (err) {
-    console.log("Error:", err);  // Log any errors
     res.status(500).json(err);
   }
 });
 
-// Get a single newsletter post by ID
+// CREATE: a new newsletter post
+router.post('/', async (req, res) => {
+  try {
+    const newsData = await Newsletter.create(req.body);
+    res.status(201).json(newsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// UPDATES: an existing newsletter using its `id`.
+router.put("/:id", async (req, res) => {
+  try {
+    const newsData = await Newsletter.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!newsData[0]) {
+      res.status(400).json({ message: "No Blog Post found with this ID." });
+      return;
+    }
+    res.status(200).json(newsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE: an existing newsletter using its `id`.
+router.delete("/:id", async (req, res) => {
+  try {
+    const newsData = await Newsletter.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!newsData) {
+      res.status(400).json({ message: "No Blog Post found with this ID." });
+      return;
+    }
+    res.status(200).json(newsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// *** WE MAY NOT NEED THIS: Get a single newsletter post by ID ***
 router.get('/:id', async (req, res) => {
   try {
     const newsletter = await Newsletter.findByPk(req.params.id);
     if (!newsletter) {
-      console.log("No newsletter found with this id!");  // Log the result
       res.status(404).json({ message: 'No newsletter found with this id!' });
       return;
     }
-    console.log("Found newsletter:", newsletter);  // Log the found newsletter
     res.status(200).json(newsletter);
   } catch (err) {
-    console.log("Error:", err);  // Log any errors
-    res.status(500).json(err);
-  }
-});
-
-// Update a newsletter post by ID
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedNewsletter = await Newsletter.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!updatedNewsletter[0]) {
-      console.log("No newsletter found with this id!");  // Log the result
-      res.status(404).json({ message: 'No newsletter found with this id!' });
-      return;
-    }
-    console.log("Updated newsletter:", updatedNewsletter);  // Log the updated newsletter
-    res.status(200).json(updatedNewsletter);
-  } catch (err) {
-    console.log("Error:", err);  // Log any errors
-    res.status(500).json(err);
-  }
-});
-
-// Delete a newsletter post by ID
-router.delete('/:id', async (req, res) => {
-  try {
-    const deletedNewsletter = await Newsletter.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!deletedNewsletter) {
-      console.log("No newsletter found with this id!");  // Log the result
-      res.status(404).json({ message: 'No newsletter found with this id!' });
-      return;
-    }
-    console.log("Deleted newsletter:", deletedNewsletter);  // Log the deleted newsletter
-    res.status(200).json(deletedNewsletter);
-  } catch (err) {
-    console.log("Error:", err);  // Log any errors
     res.status(500).json(err);
   }
 });
