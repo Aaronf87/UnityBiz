e60adc7c7c6566c4aacee008bb19ff5301b0e89c
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-09-11 20:29:47 -0500

    Updated index.js in API withing Controllers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,25 +2,23 @@ const router = require("express").Router();
 
 const companyRoutes = require("./companyRoutes");
 const employeeRoutes = require("./employeeRoutes");
-
 const newsRoutes = require("./newsRoutes");
+const poRoutes = require("./poRoutes");
+const comsRoutes = require("./comsRoutes");
 
-// *** Communication and PO routes on hold for now....
-// const comsRoutes = require("./commentRoutes");
-// const poRoutes = require("./commentRoutes");
-
-
-//Send all the requests that begin with /api/company to the companyRoutes.js in the api folder.
+// Send requests /api/company to the companyRoutes.js in the api folder.
 router.use("/company", companyRoutes);
 
-//Send all the requests that begin with /api/employee to the employeeRoutes.js in the api folder.
+// Send requests /api/employee to the employeeRoutes.js in the api folder.
 router.use("/employee", employeeRoutes);
 
-//Send all the requests that begin with /api/comments to the newsRoutes.js in the api folder.
+// Send requests /api/comments to the newsRoutes.js in the api folder.
 router.use("/news", newsRoutes);
 
-// *** Commnunication and PO routes on hold for now....
-// router.use("/coms", comRoutes);
-// router.use("/po", poRoutes);
+// Send requests /api/po to the poRoutes.js in the api folder.
+router.use("/po", poRoutes);
+
+// Send requests /api/coms to the comsRoutes.js in the api folder.
+router.use("/coms", comsRoutes);
 
 module.exports = router;
