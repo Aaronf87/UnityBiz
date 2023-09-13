e452b963eb328ea3715c0394fb4204f5a89628ad
Aaron Flores <97806359+Aaronf87@users.noreply.github.com>
2023-09-12 19:57:58 -0500

    added employee-signup on handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,20 @@
 const router = require("express").Router();
-router.get('/company-signup', async (req,res)=> {
-try{
-    res.render('company-signup')
-
-}catch(err){
+router.get("/company/signup", async (req, res) => {
+  try {
+    res.render("company-signup");
+  } catch (err) {
     res.status(500).json(err);
-}
-
-}
-)
+  }
+});
+router.get("/employee/signup", async (req, res) => {
+    try {
+      res.render("employee-signup");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 // ***WILL PASS IN MODELS THAT MATTHEW WILL MAKE
-// const {  } = require("../models"); 
+// const {  } = require("../models");
 
 // The `/` endpoint
 
